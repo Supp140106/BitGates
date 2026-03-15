3b37d54aa26a392803ac2c460f993c47e9a3c12a
quark_cephi <suppritdas@gmail.com>
2026-03-15 12:24:12 +0530

    feat: Add groq-sdk and react-markdown dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import Dragflow from './Dragflow/Dragflow'
 function App() {
   return <div style={{ width: '100vw', height: '100vh' }}>
-      <Dragflow />
-    </div>
+    <Dragflow />
+  </div>
 }
 
 export default App;
